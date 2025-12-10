66a03686dde0b9cc257fef107f407d5c54e8809f
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-11 07:24:26 +0900

    lesson 26

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { ref } from 'vue'
-const vueURL = 'ref(https://vuejs.org)'
+const vueURL = ref('https://vuejs.org');
 const vueID = ref('vue-link')
 </script>
 <template>
-  <a :id="undefined" :href="false">Vue.js</a>
+  <a v-bind="{id: vueID, href: vueURL}">Vue.js</a>
   <button :disabled="null">Button</button>
 </template>
