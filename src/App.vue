f2ae50db33086d73668c9a1d7b3c4570a5102e5a
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-11 20:38:37 +0900

    lesson 28

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup>
-  import { ref } from 'vue'
-  const count = ref(0);
-  function countUp() {
-    count.value++;
-  }
+import { ref } from 'vue'
+const count = ref(0)
+function countUp(event) {
+  console.log(event)
+  count.value++
+}
 </script>
 <template>
   <p>{{ count }}</p>
   <!-- <button v-on:click="count++">Button</button> -->
-   <!-- <button @click="count++">Button</button> -->
-   <button @click="countUp">Button</button>
+  <button @click="count = $event.clientX">Button</button>
+  <button @click="countUp">Button</button>
 </template>
