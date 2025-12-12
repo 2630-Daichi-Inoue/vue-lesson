9db108bdf12458a105c6cc37c074d1544d4004c9
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-13 08:32:54 +0900

    lesson 37

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,12 @@
 <script setup>
-  import { ref, computed } from 'vue'
-  const score = ref(0);
+  import { ref, watch, watchEffect } from 'vue'
   const count = ref(0);
-  function tmp() {
-    // console.log('tmp');
-    return score.value > 3 ? 'Good' : 'Bad';
-  }
-  const evaluation = computed(() => {
-    console.log('computed');
-    return score.value > 3 ? 'Good' : 'Bad';
+  watchEffect(() => {
+    console.log('watchEffectです');
+    console.log(count.value);
   })
-
-  const parentComp = computed(() => {
-    console.log('parentComp computed');
-    return evaluation.value;
-  })
-
-  function getEvaluation() {
-    console.log(evaluation.value);
-  }
 </script>
 <template>
-  <p>{{ score > 3 ? 'Good' : 'Bad' }}</p>
-  <!-- <p>{{ evaluation }}</p> -->
-  <p>{{ tmp() }}</p>
-  <p>{{ parentComp }}</p>
-  <p>Score: {{ score }}</p>
-  <p>Count: {{ count }}</p>
-  <button @click="score++">+1 Score</button>
-  <button @click="count++">+1 Count</button>
-  <button @click="getEvaluation">Get Evaluation</button>
+  <p>{{ count }}</p>
+  <button @click="count++">+1</button>
 </template>
