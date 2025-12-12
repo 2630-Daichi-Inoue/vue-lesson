e296edd9cddf5881dc1f662a1236f63552784ba0
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-13 08:03:50 +0900

    lesson 36

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <script setup>
   import { ref, computed } from 'vue'
   const score = ref(0);
+  const count = ref(0);
+  function tmp() {
+    // console.log('tmp');
+    return score.value > 3 ? 'Good' : 'Bad';
+  }
   const evaluation = computed(() => {
+    console.log('computed');
     return score.value > 3 ? 'Good' : 'Bad';
   })
+
+  const parentComp = computed(() => {
+    console.log('parentComp computed');
+    return evaluation.value;
+  })
+
+  function getEvaluation() {
+    console.log(evaluation.value);
+  }
 </script>
 <template>
-  <p>{{ score }}</p>
-  <p>{{ evaluation }}</p>
   <p>{{ score > 3 ? 'Good' : 'Bad' }}</p>
-  <button @click="score++">+1</button>
+  <!-- <p>{{ evaluation }}</p> -->
+  <p>{{ tmp() }}</p>
+  <p>{{ parentComp }}</p>
+  <p>Score: {{ score }}</p>
+  <p>Count: {{ count }}</p>
+  <button @click="score++">+1 Score</button>
+  <button @click="count++">+1 Count</button>
+  <button @click="getEvaluation">Get Evaluation</button>
 </template>
