d6e10bf2e81da1d8b3e8aa4f0640bfd0ab01afa8
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-13 17:51:07 +0900

    lesson 43

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,10 @@
   const maybeOk = ref(true)
 </script>
 <template>
-  <p v-if="ok">OK!</p>
-  <p v-else-if="maybeOk">Maybe OK ...</p>
-  <p v-else>No!</p>
   <button @click="ok = !ok">Toggle</button>
+  <template v-if="ok">
+  <p>OK!</p>
+  <p>Hello, Vue.js!</p>
+  <p>I'm a genius!</p>
+  </template>
 </template>
