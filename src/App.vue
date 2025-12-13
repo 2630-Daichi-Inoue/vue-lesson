3118a24520cdb51393f32d393d752f69699e9de6
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-13 17:57:17 +0900

    lesson 44

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <script setup>
   import { ref } from 'vue'
   const ok = ref(true)
-  const maybeOk = ref(true)
 </script>
 <template>
   <button @click="ok = !ok">Toggle</button>
-  <template v-if="ok">
-  <p>OK!</p>
-  <p>Hello, Vue.js!</p>
-  <p>I'm a genius!</p>
-  </template>
+  <p v-show="ok">OK!</p>
 </template>
