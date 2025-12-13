9f737fe28ae9050bcfb2401415c4675b216a4f91
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-13 18:27:00 +0900

    lesson 46

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup>
   import { ref } from 'vue'
-  const fruits = ref(['Apple', 'Banana', 'Cherry'])
+  const fruits = ref([
+      {id: 1, name: 'Apple'},
+      {id: 2, name: 'Apple'},
+      {id: 3, name: 'Banana'},
+      {id: 4, name: 'Cherry'},
+    ]
+  )
 </script>
 <template>
-  <li v-for="fruit in fruits">{{ fruit }}</li>
+  <button @click="fruits.shift()">Button</button>
+  <li v-for="fruit in fruits" :key="fruit.id"><input type="text">{{ fruit.name }}</li>
 </template>
