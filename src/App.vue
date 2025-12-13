74edc36dd8219a503b1b6085c226ab1cc2555084
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-13 09:09:33 +0900

    lesson 38

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <script setup>
   import { ref, watch, watchEffect } from 'vue'
-  const count = ref(0);
-  watchEffect(() => {
-    console.log('watchEffectです');
-    console.log(count.value);
-  })
+  const count1 = ref(0);
+  const count2 = ref(0);
+  const count3 = ref(0);
+  watch([count1, count2],(newValue, oldValue) => {
+    console.log('watchです');
+    console.log('newValue:', newValue);
+    console.log('oldValue:', oldValue);
+  },
+{
+    immediate: true
+})
+  // watchEffect(() => {
+  //   console.log('watchEffectです');
+  //   console.log(count.value);
+  // })
 </script>
 <template>
-  <p>{{ count }}</p>
-  <button @click="count++">+1</button>
+  <p>{{ count1 }}(count1)</p>
+  <p>{{ count2 }}(count2)</p>
+  <p>{{ count3 }}(count3)</p>
+  <button @click="count1++">count1++</button>
+  <button @click="count2++">count2++</button>
+  <button @click="count3++">count3++</button>
 </template>
