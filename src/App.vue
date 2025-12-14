6ae544e5404db88cd6133b67937ce9b77fc4fd4c
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-15 07:26:21 +0900

    lesson 81

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script setup>
   import { ref } from 'vue';
-  import ResetButton from './components/ResetButton.vue';
+  import CountUp from './components/CountUp.vue';
+  // import ResetButton from './components/ResetButton.vue';
 
-  const count = ref(0)
+  // const count = ref(0)
+  const isShow = ref(true)
 
-  function onReset(value) {
-    count.value = value;
-  }
+  // function onReset(value) {
+  //   count.value = value;
+  // }
 </script>
 <template>
-  <p>Count: {{ count }}</p>
-  <button @click="count++">Count Up</button>
-  <ResetButton @reset-count="onReset"/>
+
+  <button @click="isShow = !isShow">Switch</button>
+  <CountUp v-if="isShow" />
+  <!-- <ResetButton @reset-count="onReset"/> -->
 </template>
--- src/components/CountUp.vue
+++ src/components/CountUp.vue
@@ -1,11 +1,31 @@
 <script setup>
-  import { ref } from 'vue'
+  import { ref, onMounted, onUpdated, onUnmounted, onBeforeMount, onBeforeUpdate, onBeforeUnmount } from 'vue'
   const count = ref(0)
+  onBeforeMount(()=> {
+    console.log('CountUp before mount')
+  })
+  onMounted(()=> {
+    console.log('CountUp mounted')
+  })
+
+  onBeforeUpdate(()=> {
+    console.log('CountUp before update')
+  })
+  onUpdated(()=> {
+    console.log('CountUp updated')
+  })
+
+  onBeforeUnmount(()=> {
+    console.log('CountUp before unmount')
+  })
+  onUnmounted(()=> {
+    console.log('CountUp unmounted')
+  })
 </script>
 
 <template>
-  <h2 v-bind="$attrs">Count up</h2>
-  <BaseIcon />
+  <!-- <h2 v-bind="$attrs">Count up</h2>
+  <BaseIcon /> -->
   <p>count: {{ count }}</p>
   <button @click="count++">Count Up</button>
 </template>
