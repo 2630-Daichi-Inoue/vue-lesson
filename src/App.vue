d32061226b32f2abbb99b15fc3811b323178f146
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-14 14:27:46 +0900

    lesson 60

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,20 @@ import CountUp from './components/CountUp.vue'
 import BaseButton from './components/BaseButton.vue';
 </script>
 <template>
-  <h1>App</h1>
+  <h1 class="red">Hello, Vue.js!</h1>
   <BaseIcon />
   <CountUp  id="base-button"/>
   <BaseButton id="base-button" @click="console.log('App.vue')"/>
 </template>
+
+<style>
+  body {
+    background-color: beige;
+  }
+</style>
+
+<style scoped>
+  .red {
+    color: red;
+  }
+</style>
