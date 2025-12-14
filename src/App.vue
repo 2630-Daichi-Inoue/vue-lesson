60167dc02265d788c937e68ecbacbfca926c45db
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-14 16:19:53 +0900

    lesson 69

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,13 @@
   import ResetButton from './components/ResetButton.vue';
 
   const count = ref(0)
+
+  function onReset(value) {
+    count.value = value;
+  }
 </script>
 <template>
   <p>Count: {{ count }}</p>
   <button @click="count++">Count Up</button>
-  <ResetButton @reset="count = 0"/>
+  <ResetButton @reset="onReset"/>
 </template>
