ad78913497a720d7d629535dfe624a1aa74bec75
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-15 07:39:55 +0900

    lesson 82

--- src/components/CountUp.vue
+++ src/components/CountUp.vue
@@ -1,6 +1,16 @@
 <script setup>
-  import { ref, onMounted, onUpdated, onUnmounted, onBeforeMount, onBeforeUpdate, onBeforeUnmount } from 'vue'
+  import {
+    ref,
+    onMounted,
+    onUpdated,
+    onUnmounted,
+    onBeforeMount,
+    onBeforeUpdate,
+    onBeforeUnmount,
+    nextTick
+} from 'vue'
   const count = ref(0)
+  console.log('CountUp setup')
   onBeforeMount(()=> {
     console.log('CountUp before mount')
   })
@@ -21,11 +31,19 @@
   onUnmounted(()=> {
     console.log('CountUp unmounted')
   })
+  async function countUp() {
+    count.value++
+    // nextTick(() => {
+    //   console.log('DOM updated')
+    // })
+    await nextTick()
+    console.log('DOM updated')
+  }
 </script>
 
 <template>
   <!-- <h2 v-bind="$attrs">Count up</h2>
   <BaseIcon /> -->
   <p>count: {{ count }}</p>
-  <button @click="count++">Count Up</button>
+  <button @click="countUp">Count Up</button>
 </template>
