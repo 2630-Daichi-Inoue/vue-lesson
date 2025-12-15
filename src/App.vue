a7fa634383cbb76d8c2dac906c1d5f5c07d1d528
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-15 20:37:58 +0900

    lesson 85

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,10 @@
 <script setup>
-  import { ref, useTemplateRef, onMounted } from 'vue';
-  import CountUp from './components/CountUp.vue';
-  // import ResetButton from './components/ResetButton.vue';
-
-  // const count = ref(0)
-  const isShow = ref(true)
-
-  // function onReset(value) {
-  //   count.value = value;
-  // }
-  const userInput = useTemplateRef('user-inpiut')
-  console.log(userInput.value)
-  onMounted(() => {
-    console.log(userInput.value)
-  })
+  import BaseCard from './components/BaseCard.vue';
 </script>
 <template>
-
-  <input ref="user-input" type="text"><br>
-
-  <button @click="isShow = !isShow">Switch</button>
-  <CountUp v-if="isShow" />
-  <!-- <ResetButton @reset-count="onReset"/> -->
+  <h1>Slots</h1>
+  <BaseCard>
+    <h2>Hello</h2>
+    <p>How are you?</p>
+  </BaseCard>
 </template>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+  div {
+    box-shadow: 2px 2px 6px rgba(255, 255, 255, 0.5);
+    border-radius: 5px;
+    padding: 16px;
+  }
+</style>
