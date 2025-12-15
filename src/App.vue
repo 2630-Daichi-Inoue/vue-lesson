96ee1b16fa3f853de0632ac7266d2853c0f693f9
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-16 07:41:47 +0900

    lesson 89

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <script setup>
-import BaseCard from './components/BaseCard.vue'
+import { ref } from 'vue'
+import CompA from './components/CompA.vue'
+import CompB from './components/CompB.vue'
+import CompC from './components/CompC.vue'
+const currentComp = ref(CompA)
 </script>
 <template>
-  <h1>Slots</h1>
-  <BaseCard>
-    <template #header="{ pageCount }">
-      <p>pageCpunt: {{ pageCount }}</p>
-      <h2 v-if="pageCount === 1">Vue.js Course</h2>
-      <h2 v-if="pageCount === 2">HTML Course</h2>
-      <h2 v-if="pageCount === 3">CSS Course</h2>
-    </template>
-    <template #default="{ pageCount }">
-      <p v-if="pageCount === 1">This is the best Vue.js course ever.</p>
-      <p v-if="pageCount === 1">You will learn how convenient vue.js is!</p>
-      <p v-if="pageCount === 2">This is the best HTML course ever.</p>
-      <p v-if="pageCount === 2">You will learn how convenient HTML is!</p>
-      <p v-if="pageCount === 3">This is the best CSS course ever.</p>
-      <p v-if="pageCount === 3">You will learn how convenient CSS is!</p>
-    </template>
-
-    <template #footer>
-      <p>© 2024 Vue.js Academy</p>
-    </template>
-
-  </BaseCard>
-
+  <h1>Dynamic Components</h1>
+  <button @click="currentComp = CompA">A</button>
+  <button @click="currentComp = CompB">B</button>
+  <button @click="currentComp = CompC">C</button>
+  <component :is="currentComp" />
 </template>
