3af0e1d0d24e6937c6838f7fc7c7939cd97f4e2a
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-17 20:05:38 +0900

    lesson 103

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,12 @@
 <script setup>
 import { ref } from 'vue'
-const userInput = ref('')
-const message = ref('')
-const checked = ref('Not Checked')
-const fruits = ref([])
-const gender = ref('Male')
-const selected = ref('')
-
+import CustomInput from './components/CustomInput.vue'
+const userInput = ref('Hello!')
+const titleInput = ref('Title')
 </script>
 <template>
   <h1>v-model</h1>
-  <h2>Text</h2>
-  <input v-model.lazy.number="userInput" type="text">
-  <p>{{ userInput }}</p>
-
-  <h2>Textarea</h2>
-  <textarea v-model.trim="message"></textarea>
-  <p style="white-space: pre">{{ message }}</p>
-
-  <h2>Checkbox</h2>
-  <input id="checkbox" v-model="checked" type="checkbox" true-value="Checked" false-value="Not checked" />
-  <label for="checkbox">{{ checked }}</label>
-
-  <p>Fruits</p>
-  <input id="Apple" v-model="fruits" type="checkbox" value="Apple" />
-  <label for="Apple">Apple</label>
-  <input id="Banana" v-model="fruits" type="checkbox" value="Banana" />
-  <label for="Banana">Banana</label>
-  <input id="Cherry" v-model="fruits" type="checkbox" value="Cherry" />
-  <label for="Cherry">Cherry</label>
-  <p>{{ fruits }}</p>
-
-  <h2>Radio</h2>
-  <input id="Male" v-model="gender" type="radio" value="Male" />
-  <label for="Male">Male</label>
-  <input id="Female" v-model="gender" type="radio" value="Female" />
-  <label for="Female">Female</label>
-  <p>{{ gender }}</p>
-
-  <h2>Select</h2>
-  <select v-model="selected" mutiple>
-    <option value="" hidden>Select One</option>
-    <option v-for="option in ['A', 'B', 'C']" :key="option">{{ option }}</option>
-  </select>
-  <p>{{ selected }}</p>
+  <CustomInput v-model.uppercase="userInput" v-model:title-name="titleInput" />
+  <p>userInput: {{ userInput }}</p>
+  <p>titleInput: {{ titleInput }}</p>
 </template>
---
+++ src/components/CustomInput.vue
@@ -0,0 +1,26 @@
+<script setup>
+  const [model, modifiers] = defineModel({
+    type: String,
+    default: 'default',
+    set(value) {
+      if(modifiers.uppercase){
+        return value.toUpperCase()
+      } else {
+        return value
+      }
+    }
+  })
+  const titleName = defineModel('titleName', {
+    type: String,
+    required: true
+  })
+</script>
+
+<template>
+  <h2>CutomInput</h2>
+  <input v-model="model" type="text">
+  <p>model: {{ model }}</p>
+  <p>modifiers: {{ modifiers }}</p>
+  <input v-model="titleName" type="text">
+  <p>titleName: {{ titleName }}</p>
+</template>
