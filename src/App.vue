d58dcfe276b94aa2f09e0c1786a43727a00bc079
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-17 19:42:44 +0900

    lesson 102

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,11 @@ const selected = ref('')
 <template>
   <h1>v-model</h1>
   <h2>Text</h2>
-  <input v-model="userInput" type="text">
+  <input v-model.lazy.number="userInput" type="text">
   <p>{{ userInput }}</p>
 
   <h2>Textarea</h2>
-  <textarea v-model="message"></textarea>
+  <textarea v-model.trim="message"></textarea>
   <p style="white-space: pre">{{ message }}</p>
 
   <h2>Checkbox</h2>
