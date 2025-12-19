11e3e9873743947af869cff2f5e1dc87d5815069
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-20 08:08:35 +0900

    lesson 118

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,43 @@
   import { ref } from 'vue';
   const isShow = ref(true);
 
+  function beforeEnter(el) {
+    console.log('beforeEnter', el)
+  }
+
+  function enter(el) {
+    console.log('Enter', el)
+  }
+
+  function afterEnter(el) {
+    console.log('afterEnter', el)
+  }
+
+  function beforeLeave(el) {
+    console.log('beforeLeave', el)
+  }
+
+  function leave(el) {
+    console.log('Leave', el)
+  }
+
+  function afterLeave(el) {
+    console.log('afterLeave', el)
+  }
+
 </script>
 <template>
   <h1>Animation</h1>
   <button @click="isShow = !isShow">Switch</button>
-  <Transition name="fade" mode="out-in">
+  <Transition
+    name="fade"
+    @before-enter="beforeEnter"
+    @enter="enter"
+    @after-enter="afterEnter"
+    @before-leave="beforeLeave"
+    @leave="leave"
+    @after-leave="afterLeave">
     <div v-if="isShow">On</div>
-    <div v-else>Off</div>
   </Transition>
 </template>
 <style scoped>
