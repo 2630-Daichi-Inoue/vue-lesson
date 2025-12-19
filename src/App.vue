a7f26a1a2a611906e1f0d3ddaad39b99557e0c1f
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-20 08:25:25 +0900

    lesson 119

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,82 @@
 <script setup>
-  import { ref } from 'vue';
-  const isShow = ref(true);
+import { ref } from 'vue'
+const isShow = ref(true)
 
-  function beforeEnter(el) {
-    console.log('beforeEnter', el)
-  }
+function beforeEnter(el) {
+  console.log('beforeEnter', el)
+  el.style.transform = 'translateX(30px)'
+}
 
-  function enter(el) {
-    console.log('Enter', el)
-  }
+function enter(el, done) {
+  console.log('Enter', el)
+  let translateXValue = 30
+  const intervalId = setInterval(() => {
+    translateXValue--
+    el.style.transform = `translateX(${translateXValue}px)`
+    if (translateXValue === 0) {
+      clearInterval(intervalId)
+      done()
+    }
+  }, 20)
+}
 
-  function afterEnter(el) {
-    console.log('afterEnter', el)
-  }
+function afterEnter(el) {
+  console.log('afterEnter', el)
+}
 
-  function beforeLeave(el) {
-    console.log('beforeLeave', el)
-  }
+function beforeLeave(el) {
+  console.log('beforeLeave', el)
+}
 
-  function leave(el) {
-    console.log('Leave', el)
-  }
+function leave(el, done) {
+  console.log('Leave', el)
+  let translateXValue = 0
+  const intervalId = setInterval(() => {
+    translateXValue++
+    el.style.transform = `translateX(${translateXValue}px)`
+    if (translateXValue === 30) {
+      clearInterval(intervalId)
+      done()
+    }
+  }, 20)
+}
 
-  function afterLeave(el) {
-    console.log('afterLeave', el)
-  }
-
+function afterLeave(el) {
+  console.log('afterLeave', el)
+}
 </script>
 <template>
   <h1>Animation</h1>
   <button @click="isShow = !isShow">Switch</button>
   <Transition
-    name="fade"
+    :css="false"
     @before-enter="beforeEnter"
     @enter="enter"
     @after-enter="afterEnter"
     @before-leave="beforeLeave"
     @leave="leave"
-    @after-leave="afterLeave">
+    @after-leave="afterLeave"
+  >
     <div v-if="isShow">On</div>
   </Transition>
 </template>
 <style scoped>
-  .fade-enter-from{
-    opacity: 0;
-  }
-  .fade-enter-active {
-    transition: opacity 1s;
-  }
-  .fade-enter-to {
-    opacity: 1;
-  }
-  .fade-leave-from{
-    opacity: 1;
-  }
-  .fade-leave-active {
-    transition: opacity 1s;
-  }
-  .fade-leave-to {
-    opacity: 0;
-  }
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 1s;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-leave-from {
+  opacity: 1;
+}
+.fade-leave-active {
+  transition: opacity 1s;
+}
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
