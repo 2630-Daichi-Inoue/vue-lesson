1204cd19ea1d45b29558fe1280e1d321906eeec4
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-21 12:27:20 +0900

    lesson 124

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import BaseIcon from './components/BaseIcon.vue'
 import 'animate.css'
+import router from './router'
 
 const app = createApp(App)
 app.component('BaseIcon', BaseIcon)
+
+app.use(router)
 app.mount('#app')
