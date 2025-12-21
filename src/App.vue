0d4efb60e6a71a8ec4945c352b296342e8e1c1ff
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-21 13:10:33 +0900

    lesson 130

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
 </script>
 <template>
   <h1>Vue Router</h1>
-  <RouterLink :to="{ name: 'home' }" replace>Home</RouterLink>
-  <RouterLink :to="{ name: 'about' }" replace>About</RouterLink>
+  <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+  <RouterLink :to="{ name: 'about' }">About</RouterLink>
   <RouterView />
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,5 @@
 <template>
   <h2>Home</h2>
+  <h3>Blog</h3>
+  <RouterLink :to="{ name: 'blog', params: { id: 21, version: 21 }}">New Release!!!</RouterLink>
 </template>
