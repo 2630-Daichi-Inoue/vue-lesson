43c127346194602db90982b020cd0d3b8d18a092
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-21 13:19:39 +0900

    lesson 131

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,3 +1,16 @@
+<script setup>
+  import { useRoute } from 'vue-router'
+  const route = useRoute()
+  console.log(route)
+</script>
+
 <template>
-  <h2>Blog</h2>
+  <h2>Blog(id: {{ $route.params.id }})</h2>
+  <h2>Ver.: {{ $route.params.version }}</h2>
+  <RouterLink
+    :to="{
+    name: 'blog',
+    params: { id: Number($route.params.id) + 1, version: $route.params.version } }">
+    Next
+  </RouterLink>
 </template>
