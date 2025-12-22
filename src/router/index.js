513afa8353be8316dd22299ea163b03297425da9
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-23 07:36:39 +0900

    lesson 139

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,24 @@ const router = createRouter({
       name: 'blog',
       component: BlogView
     },
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    console.log(to, from, savedPosition)
+    return new Promise(resolve => {
+      setTimeout(() => {
+        if (savedPosition) {
+          return resolve(savedPosition)
+        }
+        if (to.hash) {
+          return resolve({
+            el: '#blog',
+            top: 10,
+            behavior: 'smooth'
+          })
+        }
+        return resolve({top: 0, left: 0, behavior: 'smooth'})
+      }, 2000)
+    })
+  }
 })
 export default router
