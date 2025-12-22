2922afe254ad3c865fdf58d000b8fc484d31cfb4
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-23 06:51:11 +0900

    lesson 138

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
   <h1>Vue Router</h1>
   <RouterLink :to="{ name: 'home' }">Home</RouterLink>
   <RouterLink :to="{ name: 'blog', params:{ id: 3 } }">Blog</RouterLink>
-  <RouterView v-slot="{ Component }">
+  <RouterView v-slot="{ Component, route }">
     <Transition name="fade" mode="out-in">
-      <component :is="Component" />
+      <component :is="Component" :key="route.path"/>
     </Transition>
   </RouterView>
 
