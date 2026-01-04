a0eb559503eadc8d56d811e4f63511c5219f6d35
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2026-01-04 16:04:20 +0900

    lesson 152

--- src/main.js
+++ src/main.js
@@ -6,10 +6,13 @@ import BaseIcon from './components/BaseIcon.vue'
 import 'animate.css'
 import router from './router'
 import { createPinia } from 'pinia'
+import { useCounterStore } from './stores/counter'
 
 const app = createApp(App)
 const pinia = createPinia()
 app.use(pinia)
+const counterStore = useCounterStore()
+console.log('main.js', counterStore.count)
 app.component('BaseIcon', BaseIcon)
 
 app.use(router)
